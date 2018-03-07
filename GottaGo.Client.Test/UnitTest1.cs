e34761b54c8564d262640b28dc1f4520712aebbc
Matt <matthew.johnson@inmproving.com>
2018-03-07 17:31:05 -0600

    Add test project

---
+++ GottaGo.Client.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GottaGo.Client.Test
+{
+    //[TestClass]
+    [TestFixture]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
